2994501113dfc2db6b84bfe9e6ac2268c933f9c2
Yorman Rojas <yormanxrojas@gmail.com>
2022-04-03 14:19:35 -0500

    modular structure

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [],
+  declarations: [],
+  providers: [],
+})
+
+
+
+export class AdminModule { }
